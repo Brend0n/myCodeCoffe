943bf940f82b4fc3ecb7c348a0c11ec805fe8a65
Brendon BOUZAS <brendon.bouzas@ymail.com>
2019-02-09 23:59:38 +0900

    added fizzbuzz problem

---
+++ code/fizzbuzz.js
@@ -0,0 +1,5 @@
+// write a function that print numbers 1 to 50
+// with the exception of when the number is multiple of 3 it print "fizz"
+//  or when it's a multiple of 5 it prints "buzz"
+//  and when it's a multiple of bith 3 & 5 it prints "fizzbuzz"
+module.exports = function() {};
