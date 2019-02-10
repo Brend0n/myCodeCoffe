dfba3edfa2e8c72f9aaa07dd774ffca5afb6065b
Brendon BOUZAS <brendon.bouzas@ymail.com>
2019-02-10 23:55:16 +0900

    completed the fizzbuz exercise

--- code/fizzbuzz.js
+++ code/fizzbuzz.js
@@ -1,5 +1,20 @@
 // write a function that print numbers 1 to 50
-// with the exception of when the number is multiple of 3 it print "fizz"
+// but when the number is a multiple of 3 it print "fizz"
 //  or when it's a multiple of 5 it prints "buzz"
-//  and when it's a multiple of bith 3 & 5 it prints "fizzbuzz"
-module.exports = function() {};
+//  or when it's a multiple of bith 3 & 5 it prints "fizzbuzz"
+module.exports = function() {
+  for (let i = 1; i <= 50; i++) {
+    let myStr = "";
+    if (i % 3 === 0) {
+      myStr += "fizz";
+    }
+    if (i % 5 === 0) {
+      myStr += "buzz";
+    }
+    if (myStr) {
+      console.log(myStr);
+    } else {
+      console.log(i);
+    }
+  }
+};
